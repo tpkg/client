92e82adc383bec4ed4eee62425fa3d4b4469caee
Jason Heiss <jheiss@aput.net>
2011-01-11 03:58:11 +0000

    Add --base and --test-root options
    Preserve base directory setting when running tpkg via sudo or remotely
    by using the --base option rather than "env TPKG_HOME".  This ensures
    that tpkg is still the command being executed, rather than env.  This is
    important if sudo rules only allow the user to run tpkg.
    
    Be more explicit about the precedence of the various methods of setting
    the tpkg base directory.
    
    Change the hash keys for @tpkg_options in tpkg executable from strings
    to symbols.

--- client/test/test_options.rb
+++ client/test/test_options.rb
@@ -10,6 +10,7 @@ class TpkgOptionTests < Test::Unit::TestCase
   
   def setup
     Tpkg::set_prompt(false)
+    @testroot = Tempdir.new("testroot")
   end
   
   def test_help
@@ -90,6 +91,101 @@ class TpkgOptionTests < Test::Unit::TestCase
     end
     # Make sure that tpkg did prompt for a password this time
     assert(output.any? {|line| line.include?('SSH Password (leave blank if using ssh key):')})
+  end
+  
+  def test_base
+    # Test the --base option
+    output = nil
+    clibase = Tempdir.new("tpkgbase")
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    IO.popen("ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --base #{clibase} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # Make sure the expected line is there
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_equal("Base: #{clibase}\n", baseline)
+  end
+    
+  def test_base_precedence
+    # Test precedence of various methods of setting base directory
+    
+    # TPKG_HOME ends up set in our environment due to use of the tpkg library
+    ENV.delete('TPKG_HOME')
+    
+    FileUtils.mkdir_p(File.join(@testroot, Tpkg::DEFAULT_CONFIGDIR))
+    File.open(File.join(@testroot, Tpkg::DEFAULT_CONFIGDIR, 'tpkg.conf'), 'w') do |file|
+      file.puts "base = /confbase"
+    end
+    
+    output = nil
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    # --base, TPKG_HOME and config file all set
+    IO.popen("env TPKG_HOME=/envbase ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --base /clibase --test-root #{@testroot} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # Make sure the expected line is there
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_equal("Base: #{File.join(@testroot, 'clibase')}\n", baseline)
+    
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    # TPKG_HOME and config file all set
+    IO.popen("env TPKG_HOME=/envbase ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --test-root #{@testroot} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # Make sure the expected line is there
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_equal("Base: #{File.join(@testroot, 'envbase')}\n", baseline)
+    
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    # Only config file set
+    IO.popen("ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --test-root #{@testroot} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # Make sure the expected line is there
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_equal("Base: #{File.join(@testroot, 'confbase')}\n", baseline)
+    
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    # Nothing is set
+    File.delete(File.join(@testroot, Tpkg::DEFAULT_CONFIGDIR, 'tpkg.conf'))
+    IO.popen("ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --test-root #{@testroot} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # Make sure the expected line is there
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_equal("Base: #{File.join(@testroot, Tpkg::DEFAULT_BASE)}\n", baseline)
+  end
+  
+  def test_test_root
+    # Test the --test-root option
+    output = nil
+    
+    # With --test-root the base directory will be /<testroot>/opt/tpkg
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    IO.popen("ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --test-root #{@testroot} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # Make sure the expected line is there
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_equal("Base: #{File.join(@testroot, Tpkg::DEFAULT_BASE)}\n", baseline)
+    
+    # Without --test-root the base directory will be something else (depending
+    # on what config files are on the system)
+    # The File.join(blah) is roughly equivalent to '../bin/tpkg'
+    parentdir = File.dirname(File.dirname(__FILE__))
+    IO.popen("ruby -I #{File.join(parentdir, 'lib')} #{File.join(parentdir, 'bin', 'tpkg')} --qconf") do |pipe|
+      output = pipe.readlines
+    end
+    # This is a rather lame test, but we don't have any way to know how tpkg
+    # is configured on the system on which the tests are running.
+    baseline = output.find {|line| line.include?('Base: ')}
+    assert_not_equal("Base: #{File.join(@testroot, Tpkg::DEFAULT_BASE)}\n", baseline)
   end
   
   def teardown
